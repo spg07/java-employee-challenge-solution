a81ded108daca77aa406240de855e915c09e7e70
spg07 <sharvilg07@gmail.com>
2024-10-29 16:24:19 +0530

    Updated global exception handler.

--- api/src/main/java/com/reliaquest/api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/reliaquest/api/exception/GlobalExceptionHandler.java
@@ -1,45 +1,45 @@
 package com.reliaquest.api.exception;
 
+import com.reliaquest.api.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmployeeNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, Object> handleEmployeeNotFound(EmployeeNotFoundException ex) {
+    public ErrorResponse handleEmployeeNotFound(EmployeeNotFoundException ex) {
         return createErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
     @ExceptionHandler(EmployeeCreationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, Object> handleEmployeeCreationException(EmployeeCreationException ex) {
+    public ErrorResponse handleEmployeeCreationException(EmployeeCreationException ex) {
         return createErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
     @ExceptionHandler(EmployeeDeletionException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, Object> handleEmployeeDeletionException(EmployeeDeletionException ex) {
+    public ErrorResponse handleEmployeeDeletionException(EmployeeDeletionException ex) {
         return createErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
     @ExceptionHandler(EmployeeServiceException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, Object> handleEmployeeServiceException(EmployeeServiceException ex) {
+    public ErrorResponse handleEmployeeServiceException(EmployeeServiceException ex) {
         return createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 
-    private Map<String, Object> createErrorResponse(HttpStatus status, String message) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("status", status.value());
-        response.put("error", status.getReasonPhrase());
-        response.put("message", message);
+    private ErrorResponse createErrorResponse(HttpStatus status, String message) {
+        ErrorResponse response = new ErrorResponse();
+        response.setDetails(status.name());
+        response.setMessage(message);
+        response.setTimestamp(LocalDateTime.now());
         return response;
     }
 }
